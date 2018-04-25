bf5ddb3c168b0fec6b5ea9aa389863188ca7dc8d
Waleed Hamza <waleedhamza@Waleeds-MacBook-Pro.local>
2018-04-25 15:32:14 -0400

    added object checkers in the response array

--- liri.js
+++ liri.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 var Request = require('request')
 var keys = require("./keys.js");
-
 var Twitter = require('twitter')
 var client = new Twitter({
     consumer_key: keys.twitterKeys.consumer_key,
@@ -9,11 +8,11 @@ var client = new Twitter({
     access_token_key: keys.twitterKeys.access_token_key,
     access_token_secret: keys.twitterKeys.access_token_secret
 });
-console.log('=====================');
-console.log(' ');
-console.log(client);
-console.log(' ');
-console.log('=====================');
+// console.log('=====================');
+// console.log(' ');
+// console.log(client);
+// console.log(' ');
+// console.log('=====================');
 // console.log(OMDb);
 var data = process.argv;
 // console.log(data);
@@ -48,13 +47,16 @@ var searchItem = '';
 //=====================OMDb============================//
 function findMovie(){
     if (operator === 'movie-this'){
+
+        if(searchItem === ''){searchItem = 'Mr Nobody'}
+
         var OMDb = keys.OMDbAPI.OMDbAPI 
         var request = require("request");
         var OMDbAPI = "http://www.omdbapi.com/?t=" + searchItem  + "&y=&plot=short&apikey="+OMDb;
 
     request(OMDbAPI, function(error, response, body){
         var JASON = JSON.parse(body);
-        var rotTomRating = JASON.Ratings && JASON.Ratings[1] ? JASON.Ratings[1].Value : 'no ratings';
+        var rotTomRating = JASON.Ratings && JASON.Ratings[1] ? JASON.Ratings[1].Value : 'No Rotten Tomatoes ratings';
          if (!error && response.statusCode === 200){
         console.log("")
         console.log("")
@@ -89,46 +91,45 @@ findMovie();
 
 
 //=======spotify========/
-function searchSpotify(){
- if (operator === 'spotify-this-song'){
-     if( searchItem == ''){searchItem = 'Ace of Base The Sign'}
-    
-    var Spotify = require('node-spotify-api');
-    // console.log(keys);
-    // console.log('id', keys.spotifyKey.id);
-    // console.log('secret',keys.spotifyKey.secret )
-    var spotify = new Spotify({
-        id: keys.spotifyKey.id,
-        secret: keys.spotifyKey.secret
-      });
-    //   console.log (spotify);
-
-    spotify.search({ 
-        type: 'track', 
-        query: "The Sign"}, function(error,response){
-            if (error){
-                console.log('Error accurd:  ========= '+ error);
+function searchSpotify() {
+    if (operator === 'spotify-this-song') {
+        if (searchItem === '') {
+            searchItem = 'Ace of Base The Sign'
         }
-        console.log(response.tracks)
-        console.log("")
-        console.log('====================================')
-        console.log("Artist(s): " + response.tracks.items[0].album.artists[0].name)
-        console.log("Track: " + response.tracks.items[0].name)
-        console.log("Preview URL: " + response.tracks.items[0].preview_url);
-        console.log("Album: " + response.tracks.items[0].album.name)
-        console.log('====================================')
+
+        var Spotify = require('node-spotify-api');
+        var spotify = new Spotify(
+            {id: keys.spotifyKey.id, secret: keys.spotifyKey.secret}
+        );
+
+        spotify.search({
+            type: 'track',
+            query: searchItem
+        }, function (error, response) {
+            if (error) {
+                console.log('Error accurd:  ========= ' + error);
+            }
+            var info = response
+                .tracks
+                .items[0];
+            var previewLink = info.preview_url && info.preview_url ? info.preview_url : 'No preview url available';
+            console.log("")
+            console.log('====================================')
+            console.log("Artist(s): " + info.album.artists[0].name)
+            console.log("Track: " + info.name)
+            console.log("Preview URL: " + previewLink);
+            console.log("Album: " + info.album.name)
+            console.log('====================================')
         });
-        
 
-}//end of spotify else if statement 
-}    // Do something with 'data' 
-
+    } //end of spotify else if statement
+}//end of searchSpotify Function
 searchSpotify();
 
-
 //==============readFile=============//
-var fs = require('fs');
+
 function doThis(){
+    var fs = require('fs');
     if (operator === 'do-what-it-says'){
             fs.readFile('random.txt', 'utf8' , function(error,response){
                 if (error) {
